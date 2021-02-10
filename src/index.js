a76a2c0ed71e61b9d02d1aecb1e85173e9e702f1
Controlfreak707 <controlfreak707@gmail.com>
2021-02-10 14:22:39 -0800

    Format index.js

--- src/index.js
+++ src/index.js
@@ -176,188 +176,183 @@ const setWatermark = (value) => {
 };
 
 export default {
-goosemodHandlers: {
-  onImport: async () => {
-    style = document.createElement("style");
-    style.appendChild(
-      document.createTextNode(
-        `@import url("https://raw.githack.com/NYRI4/Comfy-theme/main/BetterDiscord/comfy.theme.css");`
-      )
-    );
-    document.head.appendChild(style);
+  goosemodHandlers: {
+    onImport: async () => {
+      style = document.createElement("style");
+      style.appendChild(
+        document.createTextNode(
+          `@import url("https://raw.githack.com/NYRI4/Comfy-theme/main/BetterDiscord/comfy.theme.css");`
+        )
+      );
+      document.head.appendChild(style);
 
-    setStatusIcon(settings.statusIcon);
-    setStatusMask(settings.statusMask);
-    setComfyAvatar(settings.comfyAvatar);
-    setColoredEmoji(settings.coloredEmoji);
-    setUserButtons(settings.userButtons);
-    setChatButtons(settings.chatButtons);
-    setCircles(settings.circles);
-    setTooltips(settings.tooltips);
-    setSeperators(settings.seperators);
-    setWatermark(settings.watermark);
+      setStatusIcon(settings.statusIcon);
+      setStatusMask(settings.statusMask);
+      setComfyAvatar(settings.comfyAvatar);
+      setColoredEmoji(settings.coloredEmoji);
+      setUserButtons(settings.userButtons);
+      setChatButtons(settings.chatButtons);
+      setCircles(settings.circles);
+      setTooltips(settings.tooltips);
+      setSeperators(settings.seperators);
+      setWatermark(settings.watermark);
+    },
+
+    onLoadingFinished: async () =>
+      goosemodScope.settings.createItem("Comfy Theme", [
+        `(v${version})`,
+        {
+          type: "header",
+          text: "BetterAvatar Popout",
+        },
+        {
+          type: "toggle",
+          text: "User Modal Status Icon",
+          onToggle: (value) => {
+            settings.statusIcon = value;
+            setStatusIcon(value);
+          },
+          isToggled: () => settings.statusIcon,
+        },
+        {
+          type: "toggle",
+          text: "User Modal Status Mask",
+          onToggle: (value) => {
+            settings.statusMask = value;
+            setStatusMask(value);
+          },
+          isToggled: () => settings.statusMask,
+        },
+        {
+          type: "toggle",
+          text: "Comfy User Modal Avatar",
+          subtext:
+            "Unfortunately, text inputs are not yet avaiable for settings. Disable to use the normal radius of user modal avatars. (Not available while user modal status masks are enabled.)",
+          onToggle: (value) => {
+            settings.comfyAvatar = value;
+            setComfyAvatar(value);
+          },
+          isToggled: () => settings.comfyAvatar,
+        },
+        {
+          type: "header",
+          text: "Other Settings",
+        },
+        {
+          type: "toggle",
+          text: "Colored Emoji Picker",
+          onToggle: (value) => {
+            settings.coloredEmoji = value;
+            setColoredEmoji(value);
+          },
+          isToggled: () => settings.coloredEmoji,
+        },
+        {
+          type: "text-and-color",
+          text: "User Settings, Mute and Deafen Buttons Color",
+          subtext: "Default: rgb(9,109,192)",
+          oninput: (value) => {
+            settings.userButtons = value;
+            setUserButtons(value);
+          },
+        },
+        {
+          type: "text-and-color",
+          text: "User Buttons Color",
+          subtext: "Default: rgb(110,133,211)",
+          oninput: (value) => {
+            settings.chatButtons = value;
+            setChatButtons(value);
+          },
+        },
+        {
+          type: "toggle",
+          text: "Circles Next to Role Names",
+          onToggle: (value) => {
+            settings.circles = value;
+            setCircles(value);
+          },
+          isToggled: () => settings.circles,
+        },
+        {
+          type: "toggle",
+          text: "Tooltips",
+          onToggle: (value) => {
+            settings.tooltips = value;
+            setTooltips(value);
+          },
+          isToggled: () => settings.tooltips,
+        },
+        {
+          type: "toggle",
+          text: "Seperators",
+          onToggle: (value) => {
+            settings.seperators = value;
+            setSeperators(value);
+          },
+          isToggled: () => settings.seperators,
+        },
+        {
+          type: "toggle",
+          text: "Discord Watermark",
+          onToggle: (value) => {
+            settings.watermark = value;
+            setWatermark(value);
+          },
+          isToggled: () => settings.watermark,
+        },
+      ]),
+
+    onRemove: async () => {
+      goosemodScope.settings.removeItem("Comfy Theme");
+
+      style.remove();
+      try {
+        cssStatusIcon.remove();
+      } catch {}
+      try {
+        cssStatusMask.remove();
+      } catch {}
+      try {
+        cssComfyAvatar.remove();
+      } catch {}
+      try {
+        cssColoredEmoji.remove();
+      } catch {}
+      try {
+        cssUserButtons.remove();
+      } catch {}
+      try {
+        cssChatButtons.remove();
+      } catch {}
+      try {
+        cssCircles.remove();
+      } catch {}
+      try {
+        cssTooltips.remove();
+      } catch {}
+      try {
+        cssSeperators.remove();
+      } catch {}
+      try {
+        cssWatermark.remove();
+      } catch {}
+    },
+
+    getSettings: () => [settings],
+    loadSettings: ([_settings]) => {
+      settings = _settings;
+
+      setStatusIcon(settings.statusIcon);
+      setStatusMask(settings.statusMask);
+      setComfyAvatar(settings.comfyAvatar);
+      setColoredEmoji(settings.coloredEmoji);
+      setUserButtons(settings.userButtons);
+      setChatButtons(settings.chatButtons);
+      setCircles(settings.circles);
+      setTooltips(settings.tooltips);
+      setSeperators(settings.seperators);
+      setWatermark(settings.watermark);
+    },
   },
-
-  onLoadingFinished: async () =>
-    goosemodScope.settings.createItem("Comfy Theme", [
-      `(v${version})`,
-      {
-        type: "header",
-        text: "BetterAvatar Popout",
-      },
-      {
-        type: "toggle",
-        text: "User Modal Status Icon",
-        onToggle: (value) => {
-          settings.statusIcon = value;
-          setStatusIcon(value);
-        },
-        isToggled: () => settings.statusIcon,
-      },
-      {
-        type: "toggle",
-        text: "User Modal Status Mask",
-        onToggle: (value) => {
-          settings.statusMask = value;
-          setStatusMask(value);
-        },
-        isToggled: () => settings.statusMask,
-      },
-      {
-        type: "toggle",
-        text: "Comfy User Modal Avatar",
-        subtext:
-          "Unfortunately, text inputs are not yet avaiable for settings. Disable to use the normal radius of user modal avatars. (Not available while user modal status masks are enabled.)",
-        onToggle: (value) => {
-          settings.comfyAvatar = value;
-          setComfyAvatar(value);
-        },
-        isToggled: () => settings.comfyAvatar,
-      },
-      {
-        type: "header",
-        text: "Other Settings",
-      },
-      {
-        type: "toggle",
-        text: "Colored Emoji Picker",
-        onToggle: (value) => {
-          settings.coloredEmoji = value;
-          setColoredEmoji(value);
-        },
-        isToggled: () => settings.coloredEmoji,
-      },
-      {
-        type: "text-and-color",
-        text: "User Settings, Mute and Deafen Buttons Color",
-        subtext: "Default: rgb(9,109,192)",
-        oninput: (value) => {
-          settings.userButtons = value;
-          setUserButtons(value);
-        },
-      },
-      {
-        type: "text-and-color",
-        text: "User Buttons Color",
-        subtext: "Default: rgb(110,133,211)",
-        oninput: (value) => {
-          settings.chatButtons = value;
-          setChatButtons(value);
-        },
-      },
-      {
-        type: "toggle",
-        text: "Circles Next to Role Names",
-        onToggle: (value) => {
-          settings.circles = value;
-          setCircles(value);
-        },
-        isToggled: () => settings.circles,
-      },
-      {
-        type: "toggle",
-        text: "Tooltips",
-        onToggle: (value) => {
-          settings.tooltips = value;
-          setTooltips(value);
-        },
-        isToggled: () => settings.tooltips,
-      },
-      {
-        type: "toggle",
-        text: "Seperators",
-        onToggle: (value) => {
-          settings.seperators = value;
-          setSeperators(value);
-        },
-        isToggled: () => settings.seperators,
-      },
-      {
-        type: "toggle",
-        text: "Discord Watermark",
-        onToggle: (value) => {
-          settings.watermark = value;
-          setWatermark(value);
-        },
-        isToggled: () => settings.watermark,
-      },
-    ]),
-
-  onRemove: async () => {
-    goosemodScope.settings.removeItem("Comfy Theme");
-
-    style.remove();
-    try {
-      cssStatusIcon.remove();
-    } catch {}
-    try {
-      cssStatusMask.remove();
-    } catch {}
-    try {
-      cssComfyAvatar.remove();
-    } catch {}
-    try {
-      cssColoredEmoji.remove();
-    } catch {}
-    try {
-      cssUserButtons.remove();
-    } catch {}
-    try {
-      cssChatButtons.remove();
-    } catch {}
-    try {
-      cssCircles.remove();
-    } catch {}
-    try {
-      cssTooltips.remove();
-    } catch {}
-    try {
-      cssSeperators.remove();
-    } catch {}
-    try {
-      cssWatermark.remove();
-    } catch {}
-  },
-
-  getSettings: () => [settings],
-  loadSettings: ([_settings]) => {
-    settings = _settings;
-
-    setStatusIcon(settings.statusIcon);
-    setStatusMask(settings.statusMask);
-    setComfyAvatar(settings.comfyAvatar);
-    setColoredEmoji(settings.coloredEmoji);
-    setUserButtons(settings.userButtons);
-    setChatButtons(settings.chatButtons);
-    setCircles(settings.circles);
-    setTooltips(settings.tooltips);
-    setSeperators(settings.seperators);
-    setWatermark(settings.watermark);
-  },
-
-
-
-
-
-}
 };
