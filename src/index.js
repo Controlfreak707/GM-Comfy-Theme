d612b57485f18b53ce1ef732927563169ce3e52e
Controlfreak707 <controlfreak707@gmail.com>
2021-03-08 16:59:44 -0800

    Nyria tell me when you change the settings please lol

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import { name, version } from "../goosemodModule.json";
 
 let settings = {
   userPopoutAnimation: true,
-  unreadPillForChannelList: true,
-  pingPillForChannelList: true,
-  noScrollbars: true,
-  comfySpotifyVersion: true,
-  betterSpotifySeekBar: true,
-  avatarWithoutStatus: true,
-  avatarRoundess: 5,
+  scrollbars: false,
+  avatarStatus: false,
   avatarWidthInModals: 130,
   serverRoundess: 8,
   coloredEmojiPicker: true,
+  mentionColor: "#f04747",
+  unreadColor: "#7289da",
   mentionColorBar: "#C66262",
   mentionColorBackground: "#c662621f",
   mentionColorHover: "#c6626226",
-  spotifySeekBarColor: "#096dc0",
   userSettingsColor: "#096dc0",
   chatButtonsColor: "#6E85D3",
   circlesNextToRoleNames: true,
@@ -24,20 +20,16 @@ let settings = {
 };
 
 let styleUserPopoutAnimation;
-let styleUnreadPillForChannelList;
-let stylePingPillForChannelList;
-let styleNoScrollbars;
-let styleComfySpotifyVersion;
-let styleBetterSpotifySeekBar;
-let styleAvatarWithoutStatus;
-let styleAvatarRoundess;
+let styleScrollbars;
+let styleAvatarStatus;
 let styleAvatarWidthInModals;
 let styleServerRoundess;
 let styleColoredEmojiPicker;
+let styleMentionColor;
+let styleUnreadColor;
 let styleMentionColorBar;
 let styleMentionColorBackground;
 let styleMentionColorHover;
-let styleSpotifySeekBarColor;
 let styleUserSettingsColor;
 let styleChatButtonsColor;
 let styleCirclesNextToRoleNames;
@@ -56,116 +48,42 @@ function updateSetting(setting, value = settings[setting]) {
 
         if (value) {
           styleUserPopoutAnimation = document.createElement("style");
-          styleUserPopoutAnimation.textContent = `.headerPlaying-j0WQBV.header-2BwW8b, .headerSpotify-zpWxgT.header-2BwW8b, .headerStreaming-2FjmGz.header-2BwW8b, .headerXbox-3G-4PF.header-2BwW8b { padding-bottom: 30px;}
-          .headerPlaying-j0WQBV.header-2BwW8b:after, .headerSpotify-zpWxgT.header-2BwW8b:after, .headerStreaming-2FjmGz.header-2BwW8b:after, .headerXbox-3G-4PF.header-2BwW8b:after { background: url('https://nyri4.github.io/Comfy/assets/wave.png'); background-size: 250px 20px; animation: animate 6s linear infinite !important; opacity: 1 !important;}
-          .header-2BwW8b:after, .headerPlaying-j0WQBV:before, .headerSpotify-zpWxgT:before, .headerStreaming-2FjmGz:before, .headerXbox-3G-4PF:before { 
-              position: absolute;
-              content: "";
-              bottom: -1px;
-              left: 0;
-              width: 250px;
-              height: 20px;
-              z-index: -1;
-              animation: animate2 6s linear infinite;
-              animation-delay: 0s;
-              opacity: 0.5;
-          }
-          .headerPlaying-j0WQBV:before { background: url('https://nyri4.github.io/Comfy/assets/wave_playing.png'); background-size: 250px 20px;}
-          .headerSpotify-zpWxgT:before { background: url('https://nyri4.github.io/Comfy/assets/wave_spotify.png'); background-size: 250px 20px;}
-          .headerStreaming-2FjmGz:before { background: url('https://nyri4.github.io/Comfy/assets/wave_streaming.png'); background-size: 250px 20px;}
-          .headerXbox-3G-4PF:before { background: url('https://nyri4.github.io/Comfy/assets/wave_xbox.png'); background-size: 250px 20px;}
-          @keyframes animate { 0% { background-position-x: 0;} 100% { background-position-x: 250px;}}
-          @keyframes animate2 {0% { background-position-x: 250px; } 100% { background-position-x: 0px;}}`;
+          styleUserPopoutAnimation.textContent = `.headerPlaying-j0WQBV.header-2BwW8b, .headerSpotify-zpWxgT.header-2BwW8b, .headerStreaming-2FjmGz.header-2BwW8b, .headerXbox-3G-4PF.header-2BwW8b { padding-bottom: 30px;}.headerPlaying-j0WQBV.header-2BwW8b:after, .headerSpotify-zpWxgT.header-2BwW8b:after, .headerStreaming-2FjmGz.header-2BwW8b:after, .headerXbox-3G-4PF.header-2BwW8b:after { background: url('https://nyri4.github.io/Comfy/assets/wave.png'); background-size: 250px 20px; animation: animate 6s linear infinite !important; opacity: 1 !important;}.header-2BwW8b:after, .headerPlaying-j0WQBV:before, .headerSpotify-zpWxgT:before, .headerStreaming-2FjmGz:before, .headerXbox-3G-4PF:before { position: absolute;content: "";bottom: -1px;left: 0;width: 250px;height: 20px;z-index: -1;animation: animate2 6s linear infinite;animation-delay: 0s;opacity: 0.5;}.headerPlaying-j0WQBV:before { background: url('https://nyri4.github.io/Comfy/assets/wave_playing.png'); background-size: 250px 20px;}.headerSpotify-zpWxgT:before { background: url('https://nyri4.github.io/Comfy/assets/wave_spotify.png'); background-size: 250px 20px;}.headerStreaming-2FjmGz:before { background: url('https://nyri4.github.io/Comfy/assets/wave_streaming.png'); background-size: 250px 20px;}.headerXbox-3G-4PF:before { background: url('https://nyri4.github.io/Comfy/assets/wave_xbox.png'); background-size: 250px 20px;}@keyframes animate { 0% { background-position-x: 0;} 100% { background-position-x: 250px;}}@keyframes animate2 {0% { background-position-x: 250px; } 100% { background-position-x: 0px;}}`;
           document.head.appendChild(styleUserPopoutAnimation);
         }
         break;
-      case "unreadPillForChannelList":
+      case "scrollbars":
         try {
-          styleUnreadPillForChannelList.remove();
+          styleScrollbars.remove();
         } catch {}
 
         if (value) {
-          styleUnreadPillForChannelList = document.createElement("style");
-          styleUnreadPillForChannelList.textContent = `.unread-2lAfLh { top: 54%; left: 5px; border-radius: 10px;width: 6px; height: 6px; background-color: #7289da; box-shadow: 0 0 12px 0 #7289da;}`;
-          document.head.appendChild(styleUnreadPillForChannelList);
-        }
-        break;
-      case "pingPillForChannelList":
-        try {
-          stylePingPillForChannelList.remove();
-        } catch {}
-
-        if (value) {
-          stylePingPillForChannelList = document.createElement("style");
-          stylePingPillForChannelList.textContent = `.mentionsBadge-3tC7Mi {position: absolute; top: 26%;right: 0px;} .mentionsBadge-3tC7Mi .numberBadge-2s8kKX { color: #f04747; font-weight: bolder;background-color: transparent !important;}.modeUnread-1qO3K1:hover .iconItem-F7GRxr { display: none;}.content-1x5b-n:active { transform: none;}`;
-          document.head.appendChild(stylePingPillForChannelList);
-        }
-        break;
-      case "noScrollbars":
-        try {
-          styleNoScrollbars.remove();
-        } catch {}
-
-        if (value) {
-          styleNoScrollbars = document.createElement("style");
-          styleNoScrollbars.textContent = `::-webkit-scrollbar { display: none !important;}.note-3HfJZ5 { margin-right: 0; }.content-1x5b-n { margin: 0 !important; border-radius: 0; }.mainContent-u_9PKf { padding-left: 8px;}.member-3-YXUe, [id*="private-channels-"] { margin: 0; max-width: unset; }.layout-2DM8Md { border-radius: 0; padding: 0 16px;}.unread-2lAfLh { z-index: 1;}.content-1LAB8Z, .item-1tOPte { margin-right: 8px;}.scroller-2hZ97C { padding-left: 0;}.scroller-2hZ97C > .content-3YMskv, .buttons-3JBrkn, .messagesPopout-24nkyi { padding-right: 10px !important; }.inviteRow-2L02ae {border-radius: 0; padding-left: 15px;}`;
-          document.head.appendChild(styleNoScrollbars);
-        }
-        break;
-      case "comfySpotifyVersion":
-        try {
-          styleComfySpotifyVersion.remove();
-        } catch {}
-
-        if (value) {
-          styleComfySpotifyVersion = document.createElement("style");
-          styleComfySpotifyVersion.textContent = `.container-6sXIoE{position: fixed;transition: left 0.5s;bottom: 62px;left: -196px;z-index: 3;width: 265px;background: var(--background-secondary-alt);border-radius: 0 5px 5px 0;border-bottom: none !important;}.barText-lmqc5O { display: none !important;}.container-6sXIoE > .inner-WRV6k5 { flex-direction: row-reverse;}.inner-WRV6k5 > .flex-1xMQg5 > .button-14-BFJ { margin: 0;}.container-6sXIoE:hover { left: 0px;}`;
-          document.head.appendChild(styleComfySpotifyVersion);
-        }
-        break;
-      case "betterSpotifySeekBar":
-        try {
-          styleBetterSpotifySeekBar.remove();
-        } catch {}
-
-        if (value) {
-          styleBetterSpotifySeekBar = document.createElement("style");
-          styleBetterSpotifySeekBar.textContent = `.bar-g2ZMIm {position: absolute !important;left: -2px !important;bottom: -4px !important;width: 267px !important;height: 58px !important;-webkit-mask-image: linear-gradient(0.25turn, #0009, #0003);mask-image: linear-gradient(0.25turn, #0009, #0003);}.maximized-vv2Wr0 > .timeline-UWmgAx > .bar-g2ZMIm { height: 84px !important; }.barFill-Dhkah7, .timeline-UWmgAx:hover .barFill-Dhkah7 { background: var(--spotify-color) !important;}.inner-WRV6k5 { z-index: 1 !important;}.grabber-7sd5f5 { display: none !important;}.button-14-BFJ:hover { background: none !important;}.container-6sXIoE:not(.maximized-vv2Wr0) > .inner-WRV6k5 > .coverWrapper-YAplwJ { margin-right: 1px !important; margin-left:25px !important;width: 40px !important;height: 40px !important;}`;
-          document.head.appendChild(styleBetterSpotifySeekBar);
-        }
-        break;
-      case "avatarWithoutStatus":
-        try {
-          styleAvatarWithoutStatus.remove();
-        } catch {}
-
-        if (value) {
-          styleAvatarWithoutStatus = document.createElement("style");
-          styleAvatarWithoutStatus.textContent = `:root {
-            --avatar-mask: none;
-            --avatar-status: 0;
-          }`;
-          document.head.appendChild(styleAvatarWithoutStatus);
+          // Nothing :V
         } else {
-          styleAvatarWithoutStatus = document.createElement("style");
-          styleAvatarWithoutStatus.textContent = `:root {
-            --avatar-mask: url(#svg-mask-avatar-status-round-32);
-            --avatar-status: 1;
-          }`;
-          document.head.appendChild(styleAvatarWithoutStatus);
+          styleScrollbars = document.createElement("style");
+          styleScrollbars.textContent = `::-webkit-scrollbar { display: none !important;}.note-3HfJZ5 { margin-right: 0; }.content-1x5b-n { margin: 0 !important; border-radius: 0; }.mainContent-u_9PKf { padding-left: 8px;}.member-3-YXUe, [id*="private-channels-"] { margin: 0; max-width: unset; }.layout-2DM8Md { border-radius: 0; padding: 0 16px;}.unread-2lAfLh { z-index: 1;}.content-1LAB8Z, .item-1tOPte { margin-right: 8px;}.scroller-2hZ97C { padding-left: 0;}.scroller-2hZ97C > .content-3YMskv, .buttons-3JBrkn, .messagesPopout-24nkyi { padding-right: 10px !important; }.inviteRow-2L02ae {border-radius: 0; padding-left: 15px;}`;
+          document.head.appendChild(styleScrollbars);
         }
         break;
-      case "avatarRoundess":
+      case "avatarStatus":
         try {
-          styleAvatarRoundess.remove();
+          styleAvatarStatus.remove();
         } catch {}
 
         if (value) {
-          styleAvatarRoundess = document.createElement("style");
-          styleAvatarRoundess.textContent = `:root {
-            --avatar-radius: ${value}%;
+          styleAvatarStatus = document.createElement("style");
+          styleAvatarStatus.textContent = `:root {
+            --avatar-mask: url(#svg-mask-avatar-status-round-32);
+            --avatar-status: 5px;
           }`;
-          document.head.appendChild(styleAvatarRoundess);
+          document.head.appendChild(styleAvatarStatus);
+        } else {
+          styleAvatarStatus = document.createElement("style");
+          styleAvatarStatus.textContent = `:root {
+            --avatar-mask: none;
+            --avatar-radius: 5px;
+          }`;
+          document.head.appendChild(styleAvatarStatus);
         }
         break;
       case "avatarWidthInModals":
@@ -176,8 +94,8 @@ function updateSetting(setting, value = settings[setting]) {
         if (value) {
           styleAvatarWidthInModals = document.createElement("style");
           styleAvatarWidthInModals.textContent = `:root {
-            --avatar-width: ${value}px;
-          }`;
+              --avatar-width: ${value}px;
+            }`;
           document.head.appendChild(styleAvatarWidthInModals);
         }
         break;
@@ -211,6 +129,32 @@ function updateSetting(setting, value = settings[setting]) {
             --colored-emoji: grayscale(100%);
           }`;
           document.head.appendChild(styleColoredEmojiPicker);
+        }
+        break;
+      case "mentionColor":
+        try {
+          styleMentionColor.remove();
+        } catch {}
+
+        if (value) {
+          styleMentionColor = document.createElement("style");
+          styleMentionColor.textContent = `:root {
+            --mention-color: ${value};
+          }`;
+          document.head.appendChild(styleMentionColor);
+        }
+        break;
+      case "unreadColor":
+        try {
+          styleUnreadColor.remove();
+        } catch {}
+
+        if (value) {
+          styleUnreadColor = document.createElement("style");
+          styleUnreadColor.textContent = `:root {
+              --unread-color: ${value};
+            }`;
+          document.head.appendChild(styleUnreadColor);
         }
         break;
       case "mentionColorBar":
@@ -252,19 +196,6 @@ function updateSetting(setting, value = settings[setting]) {
           document.head.appendChild(styleMentionColorHover);
         }
         break;
-      case "spotifySeekBarColor":
-        try {
-          styleSpotifySeekBarColor.remove();
-        } catch {}
-
-        if (value) {
-          styleSpotifySeekBarColor = document.createElement("style");
-          styleSpotifySeekBarColor.textContent = `:root {
-            --spotify-color: ${value} !important;
-          }`;
-          document.head.appendChild(styleSpotifySeekBarColor);
-        }
-        break;
       case "userSettingsColor":
         try {
           styleUserSettingsColor.remove();
@@ -299,7 +230,7 @@ function updateSetting(setting, value = settings[setting]) {
         if (value) {
           styleCirclesNextToRoleNames = document.createElement("style");
           styleCirclesNextToRoleNames.textContent = `:root {
-            --role-circle: 7px;
+            --role-circle: 5px;
           }`;
           document.head.appendChild(styleCirclesNextToRoleNames);
         } else {
@@ -394,7 +325,6 @@ export default {
           --deprecated-card-bg: #12141f63;
           --background-floating: #101320;
           --deprecated-quickswitcher-input-background:#101320;
-          --spotify-color: #096dc0;
           --elevation-low: none;
           --scrollbar-auto-thumb: #121722;
           --scrollbar-auto-track: #191f2e;
@@ -406,6 +336,7 @@ export default {
           --background-secondary: #1e2233;
           --background-secondary-alt: #191f2e;
           --background-primary: #23283d;
+          --background-accent: #6E85D3;
           --header-primary: #fff;
           --header-secondary: #b1b5b9;
           --text-normal: #8e9297;
@@ -428,49 +359,37 @@ export default {
         },
         {
           type: "toggle",
-          text: "Unread Pill For Channel List",
-          onToggle: (value) => updateSetting("unreadPillForChannelList", value),
-          isToggled: () => settings.unreadPillForChannelList,
-        },
-        {
-          type: "toggle",
-          text: "Ping Pill For Channel List",
-          onToggle: (value) => updateSetting("pingPillForChannelList", value),
-          isToggled: () => settings.pingPillForChannelList,
-        },
-        {
-          type: "toggle",
           text: "No Scrollbars",
-          onToggle: (value) => updateSetting("noScrollbars", value),
-          isToggled: () => settings.noScrollbars,
-        },
-        {
-          type: "toggle",
-          text: "Comfy Spotify Version",
-          onToggle: (value) => updateSetting("comfySpotifyVersion", value),
-          isToggled: () => settings.comfySpotifyVersion,
-        },
-        {
-          type: "toggle",
-          text: "Better Spotify Seek Bar",
-          onToggle: (value) => updateSetting("betterSpotifySeekBar", value),
-          isToggled: () => settings.betterSpotifySeekBar,
-        },
-        {
-          type: "header",
-          text: "Other Settings",
+          onToggle: (value) => updateSetting("scrollbars", value),
+          isToggled: () => settings.scrollbars,
         },
         {
           type: "toggle",
           text: "Avatar Without Status",
-          onToggle: (value) => updateSetting("avatarWithoutStatus", value),
-          isToggled: () => settings.avatarWithoutStatus,
+          onToggle: (value) => updateSetting("avatarStatus", value),
+          isToggled: () => settings.avatarStatus,
         },
         {
           type: "toggle",
           text: "Colored Emoji Picker",
           onToggle: (value) => updateSetting("coloredEmojiPicker", value),
           isToggled: () => settings.coloredEmojiPicker,
+        },
+        {
+          type: "header",
+          text: "Other Settings",
+        },
+        {
+          type: "text-and-color",
+          text: "Mention Color",
+          oninput: (value) => updateSetting("mentionColor", value),
+          initialValue: () => settings.mentionColor,
+        },
+        {
+          type: "text-and-color",
+          text: "Unread Color",
+          oninput: (value) => updateSetting("unreadColor", value),
+          initialValue: () => settings.unreadColor,
         },
         {
           type: "text-and-color",
@@ -489,12 +408,6 @@ export default {
           text: "Mention Color Hover",
           oninput: (value) => updateSetting("mentionColorHover", value),
           initialValue: () => settings.mentionColorHover,
-        },
-        {
-          type: "text-and-color",
-          text: "Spotify Seek Bar Color",
-          oninput: (value) => updateSetting("spotifySeekBarColor", value),
-          initialValue: () => settings.spotifySeekBarColor,
         },
         {
           type: "text-and-color",
